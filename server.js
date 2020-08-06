d9f35d5dba4f028dbcef09cfcfd4fe4ec68eda65
Andrew Collier <atc3598@icloud.com>
2020-08-06 19:04:51 -0400

    updated server.js to serve react build folder

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
+const path = require("path");
 const app = express();
 
 const PORT = process.env.PORT || 3001;
@@ -8,11 +8,18 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(express.static("client/build"));
+
+
 app.get("/api/config", (req, res) => {
   res.json({
     success: true,
   });
 });
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+})
 
 mongoose
   .connect(process.env.MONGODB_URI || "mongodb://localhost/react-portfolio", {
